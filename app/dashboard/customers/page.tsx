27cda280a41aaa716fab2404dbb57f8662a0c599
JSmith AR <91075107+smithb2chat@users.noreply.github.com>
2024-10-03 17:32:22 -0500

    chapter 6

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+type pageProps = {}
+
+const page: React.FC<pageProps> = ({  }) => {
+  
+  return <p>Customer page</p>
+}
+
+export default page
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+type pageProps = {}
+
+const page: React.FC<pageProps> = ({  }) => {
+  
+  return <p>Dashboard page</p>
+}
+
+export default page
